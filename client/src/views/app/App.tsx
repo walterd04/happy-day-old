5970d6876873207c9de64be8de752204c4d78471
Devin Walter <walterd04@mounties.mansfield.edu>
2020-01-24 20:00:24 -0600

    started to add to Auth0 coming up with loading problem that was supposed to be fixed and its not

--- client/src/views/app/App.tsx
+++ client/src/views/app/App.tsx
@@ -1,9 +1,29 @@
+// @ts-nocheck
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import NavBar from "./NavBar";
+import { useAuth0 } from "./react-auth0-spa";
+
 const App = () => {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
-    <div>This is the initial app rendered client side and server side</div>
+    <div>
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <Route path="/profile" />
+        </Switch>
+      </Router>
+    </div>
   );
 };
 
--- client/src/views/app/index.tsx
+++ client/src/views/app/index.tsx
@@ -1,8 +1,29 @@
-//@ts-nocheck
 import React from "react";
 import { render } from "react-dom";
 import App from "./App";
 
+import { Auth0Provider } from "./react-auth0-spa";
+import config from "./auth_config.json";
+import history from "./utils/history";
+
 const container = document.getElementById("root");
 
-render(<App {...window.props} />, container);
+const onRedirectCallback = (appState: any) => {
+  history.push(
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname,
+  );
+};
+
+render(
+  <Auth0Provider
+    domain={config.domain}
+    client_id={config.clientId}
+    redirect_uri={window.location.origin}
+    onRedirectCallback={onRedirectCallback}
+  >
+    <App />
+  </Auth0Provider>,
+  container,
+);
