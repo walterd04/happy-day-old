45dcf0fec0230ce04a685b2239da8e8e780f815d
Devin Walter <walterd04@mounties.mansfield.edu>
2020-01-25 15:19:03 -0600

    Feature/setup user auth frontend (#1)
    * started to add to Auth0 coming up with loading problem that was supposed to be fixed and its not
    
    * added auth0 context for user authentication, everything working as expected

--- client/src/views/app/App.tsx
+++ client/src/views/app/App.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import { useAuth0 } from "./react-auth0-spa";
+
 const App = () => {
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    isInitializing,
+  } = useAuth0();
+
+  if (isInitializing) {
+    return (
+      <div>
+        <h1>This is the happy-day app server side rendered loading.</h1>
+      </div>
+    );
+  }
+
   return (
-    <div>This is the initial app rendered client side and server side</div>
+    <div>
+      {!isAuthenticated && (
+        <button onClick={() => loginWithRedirect({})}>Log in</button>
+      )}
+
+      {isAuthenticated && <button onClick={() => logout()}>Log out</button>}
+    </div>
   );
 };
 
